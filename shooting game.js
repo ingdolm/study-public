2d5e2ecbaa647d732a2b97973a1dd13332c45700
Dolim Lee <ing_dolm@naver.com>
2022-09-14 16:15:36 +0900

    game

--- shooting game.js
+++ shooting game.js
@@ -33,6 +33,36 @@ function loadImage(){
     gameoverImg.src = "imgs/game-over.png"
 }
 
+let keysDown = {};
+function setupKeyboard(){
+    document.addEventListener("keydown", function(event){
+        // console.log("어떤 키보드를 눌렀나?", event.keyCode);
+        keysDown[event.keyCode] = true;
+        // console.log("keysDown에 들어간 값?", keysDown);
+    });
+    document.addEventListener("keyup",function(){
+        delete keysDown[event.keyCode];
+        // console.log("keyup 후 삭제됐나?",keysDown);
+    });
+}
+
+function update(){
+    if( 39 in keysDown){
+        spaceshipX += 5;
+    } //우주선 right 이동
+    if(37 in keysDown){
+        spaceshipX -= 5;
+    } //우주선 left 이동
+
+    if(spaceshipX <= 0){
+        spaceshipX = 0;
+    }
+    if(spaceshipX >= canvas.width-64){
+        spaceshipX = canvas.width-64;
+    }
+    //우주선x값이 0보다 크거나 작아지면 캔버스 밖으로 못나가게 고정
+}
+
 
 function render(){
     ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
@@ -40,11 +70,19 @@ function render(){
 }
 
 function main(){
+    update();
+    //좌표값 업데이트
+
     render();
+    //spaceshipImg 이동
     // console.log("animation calls main function")
     requestAnimationFrame(main);
 }
 
 
 loadImage();
+setupKeyboard();
 main();
+
+
+
